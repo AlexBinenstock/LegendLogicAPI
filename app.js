de80d5756d80f9ad5d91dea02581a0cab3003f9b
Alex Binenstock <alex.binenstock@gmail.com>
2023-06-11 12:16:34 -0500

    shell routes, added launch

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+//  import express
+const express = require('express');
 
+//  initialize express app
+const app = express();
+
+//  port number
+const port = 3000;
+
+//  use express js middleware to parse json boddies
+app.use(express.json());
+
+// simple GET route for testing
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+//  Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}); 
