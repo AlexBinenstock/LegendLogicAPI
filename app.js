e25e1a3159c0c666295c703ef102c15934b817ca
Alex Binenstock <alex.binenstock@gmail.com>
2023-06-11 12:29:01 -0500

    wip setup

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //  import express
-const express = require('express');
+const express = require("express");
 
 //  initialize express app
 const app = express();
@@ -11,11 +11,40 @@ const port = 3000;
 app.use(express.json());
 
 // simple GET route for testing
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+
+// Host a game
+app.post("/host", (req, res) => {
+  //  TODO: generate a new game code, and store in database
+
+  let gameCode = "ABC123"; // Replace with actual game code
+
+  res.status(201).json({ gameCode: gameCode });
+});
+
+// Join a game
+app.post("/join", (req, res) => {
+    let gameCode = req.body.gameCode;
+
+    // TODO: look up game code in database, and join the user to the game
+    res.status(200).json({message: 'Successfully joined game'});
+});
+
+// Get the game state
+app.get("/game/:gameCode", (req, res) => {
+    let gameCode = req.params.gameCode;
+
+    // TODO: look up game code in database, and return the game state
+
+    let gameState = {}; // Replace with actual game state
+
+    res.status(200).json(gameState);
 });
 
 //  Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-}); 
+  console.log(`Server is running on port ${port}`);
+});
